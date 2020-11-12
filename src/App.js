c2381ce51d26548be09d41c79f59cf8df5eab19e
alihirano <alihirano10@gmail.com>
2020-11-12 07:39:01 +0700

    mini project : spotify web

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Login from "./components/login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./container/player";
+import { useDataLayerValue } from "./datalayer";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [{ token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+
+      spotify.getPlaylist("37i9dQZF1DX8vAahjzdXGC").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discoverWeekly: response,
+        })
+      );
+    }
+  }, [dispatch]);
+  return (
+    <div className="App">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
+}
+
+export default App;
